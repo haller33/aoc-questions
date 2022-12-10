58858b2529c133c537900c94609ed9653980837d
haller33 <eliseu42lucena@gmail.com>
2022-12-10 12:50:05 -0300

    Conversion scripts to aoc html local files.

--- convert_markdown.sh
+++ convert_markdown.sh
@@ -1,15 +1,54 @@
 #!/bin/bash
 
-for i in $(ls aoc | grep -v 'static'); do
-	for q in $(ls aoc/$i/day | sort --version-sort); do
-		dayofyeardir="aoc/$i/day/$q"
-		echo "$dayofyeardir"
-		
-		file=$(ls $dayofyeardir | grep index )
-		inputof=$(ls $dayofyeardir | grep input )
+MARK_SIMBLE="#"
 
-		cat $dayofyeardir'/'$file
-		break;
-	done
-	break;
+markdowndir=markdown
+
+mkdir -p ${markdowndir}
+
+for i in $(ls aoc | grep -v 'static'); do
+    for q in $(ls aoc/$i/day | sort --version-sort); do
+	dayofyeardirmkd="$markdowndir/$i/day/$q"
+	dayofyeardir="aoc/$i/day/$q"
+	echo "$dayofyeardir"
+	echo "$dayofyeardirmkd"
+
+	file=$(ls $dayofyeardir | grep index )
+	inputof=$(ls $dayofyeardir | grep input )
+
+	fileinput=${dayofyeardir}'/'${inputof}
+
+	fileinputdestiny=${dayofyeardirmkd}'/'input
+
+	filehtml=${dayofyeardir}'/'${file}
+	# echo ${filehtml}
+	
+	mkdir -p ${dayofyeardirmkd}
+
+	file_md_path="$dayofyeardirmkd/index.md"
+
+	cp ${fileinput} ${fileinputdestiny}
+	
+	links -dump ${filehtml} > ${file_md_path}
+
+	
+	title=$(grep '\-\-\- Day' $file_md_path )
+
+	echo "${MARK_SIMBLE} $title" > ${file_md_path}.new
+
+	
+	## to check the higer file lenght
+	#  find . -type f -and -name '*.md' | xargs wc -l | awk '{print $1}' | uniq | sort --version-sort | tail -n 2
+
+	
+	cat ${file_md_path} | awk 'c&&c--;/Day/{c=512}' | head -n -3 >> ${file_md_path}.new
+
+	rm ${file_md_path}
+	
+	mv ${file_md_path}.new ${file_md_path}
+	
+	
+	# break;
+    done
+    # break;
 done
---
+++ convert_orgmode.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+MARK_SIMBLE="*"
+
+orgdir="org-mode"
+
+mkdir -p ${orgdir}
+
+for i in $(ls aoc | grep -v 'static'); do
+    for q in $(ls aoc/$i/day | sort --version-sort); do
+	dayofyeardirmkd="${orgdir}/$i/day/$q"
+	dayofyeardir="aoc/$i/day/$q"
+	echo "$dayofyeardir"
+	echo "$dayofyeardirorg"
+
+
+	file=$(ls $dayofyeardir | grep index )
+	inputof=$(ls $dayofyeardir | grep input )
+
+	fileinput=${dayofyeardir}'/'${inputof}
+
+	fileinputdestiny=${dayofyeardirmkd}'/'input
+
+	filehtml=${dayofyeardir}'/'${file}
+	# echo ${filehtml}
+
+	
+	mkdir -p ${dayofyeardirmkd}
+
+	file_org_path="$dayofyeardirmkd/index.org"
+
+	cp ${fileinput} ${fileinputdestiny}
+	
+	links -dump ${filehtml} > ${file_org_path}
+
+	
+	title=$(grep '\-\-\- Day' $file_org_path )
+
+	echo "${MARK_SIMBLE} $title" > ${file_org_path}.new
+
+	
+	## to check the higer file lenght
+	#  find . -type f -and -name '*.org' | xargs wc -l | awk '{print $1}' | uniq | sort --version-sort | tail -n 2
+
+	
+	cat ${file_org_path} | awk 'c&&c--;/Day/{c=512}' | head -n -3 >> ${file_org_path}.new
+
+	rm ${file_org_path}
+	
+	mv ${file_org_path}.new ${file_org_path}
+	
+	# break;
+    done
+    # break;
+done
